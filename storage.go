2858aa1f0af42b467a9088994a9d5e3478867f30
iwittkau <iwittkau@users.noreply.github.com>
2022-11-11 21:43:05 +0100

    Add Storage.AddUser and corresponding test

--- storage.go
+++ storage.go
@@ -1,8 +1,14 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
-var ErrUserNotFound = errors.New("couldn't find user")
+var (
+	ErrUserNotFound      = errors.New("couldn't find user")
+	ErrUserAlreadyExists = errors.New("user already exists")
+)
 
 type User struct {
 	ID   int
@@ -31,4 +37,14 @@ func (s *Storage) FindUser(id int) (User, error) {
 	}
 
 	return User{}, ErrUserNotFound
+}
+
+// AddUser adds a user with the provided id and name. If a user with the provided name already exists, AddUser
+// returns an ErrUserAlreadyExists.
+func (s *Storage) AddUser(id int, name string) error {
+	if _, found := s.users[name]; found {
+		return fmt.Errorf("%w: %q", ErrUserAlreadyExists, name)
+	}
+	s.users[name] = User{ID: id, Name: name}
+	return nil
 }
--- storage_test.go
+++ storage_test.go
@@ -59,3 +59,61 @@ func TestStorage_FindUser(t *testing.T) {
 		})
 	}
 }
+
+func TestStorage_AddUser(t *testing.T) {
+	// t.Parallel() signals that this test is to be run in parallel with (and only with) other parallel tests.
+	t.Parallel()
+
+	tests := map[string]struct {
+		users       map[string]User // The given underlying user storage.
+		id          int             // The user id to add.
+		name        string          // The user name to add.
+		expectedErr error           // The error that is expected to be returned.
+	}{
+		// In case the user doesn't exist, AddUser is expected to return no error.
+		"user doesn't exist": {
+			users: map[string]User{
+				"User #1": {ID: 1, Name: "User #1"},
+				"User #2": {ID: 2, Name: "User #2"},
+				"User #3": {ID: 3, Name: "User #3"},
+			},
+			id:          4,
+			name:        "User #4",
+			expectedErr: nil,
+		},
+		// In case of an already existing user, AddUser is expected to return an error.
+		"user exists": {
+			users: map[string]User{
+				"User #1": {ID: 1, Name: "User #1"},
+			},
+			id:          1,
+			name:        "User #1",
+			expectedErr: ErrUserAlreadyExists,
+		},
+	}
+
+	for name, test := range tests {
+		// Assign a new local variable because all tests will run in parallel.
+		// Otherwise this can lead to unexpected test results, because the test value gets overwritten by the
+		// next for loop iteration while a previous tests is already running. This is called "race condition".
+		// If you comment this line out, you may find that running `go test -cover ./...` doesn't yield 100% anymore.
+		test := test
+
+		t.Run(name, func(t *testing.T) {
+			// Call t.Parallel() in every subtest, so all subtests can run in parallel.
+			t.Parallel()
+
+			// Create the storage using the underlying storage provided by the test case.
+			storage := NewStorage(test.users)
+
+			// Perform the call using the given arguments.
+			err := storage.AddUser(test.id, test.name)
+
+			// Use errors.Is to determine if the expected error was returned.
+			// This also works with nil errors.
+			if !errors.Is(err, test.expectedErr) {
+				t.Fatalf("error expectancy doesn't match: expectedErr is %v, but got %v", test.expectedErr, err)
+			}
+		})
+	}
+}
